b44c2a56565c8fc1ab04f586e739de2121ca5789
Timothy Gallion <timbama@gmail.com>
2023-12-23 10:34:33 -0500

    switch to new `mkProtoDerivation` based generation

--- flake.nix
+++ flake.nix
@@ -4,27 +4,25 @@
     flake-utils.url = "github:numtide/flake-utils";
     nix-proto.url = "github:notalltim/nix-proto";
     nix-proto.inputs.nixpkgs.follows = "nixpkgs";
-
   };
   outputs = { self, nixpkgs, flake-utils, nix-proto, ... }@inputs:
     let
-      namespace_meta = nix-proto.generateMeta {
-        name = "name";
-        dir = ./proto;
+      namespace_meta_drv = nix-proto.mkProtoDerivation {
+        name = "name_stuff";
+        src = nix-proto.lib.srcFromNamespace { root = ./proto; namespace = "outer/inner/namespaced"; };
         version = "1.0.0";
         protoDeps = [ ];
-        namespace = "outer/inner/namespaced";
       };
-      meta = nix-proto.generateMeta {
+      meta_drv = { name_stuff }: nix-proto.mkProtoDerivation {
         name = "upstream";
-        dir = ./proto;
+        src = nix-proto.lib.srcFromNamespace { root = ./proto; namespace = "upstream/v1"; };
         version = "1.0.0";
-        protoDeps = [ namespace_meta ];
+        protoDeps = [ name_stuff ];
       };
-      overlays = nix-proto.generateOverlays { metas = [ meta namespace_meta ]; };
+      overlays = nix-proto.generateOverlays' { name_stuff = namespace_meta_drv; upstream = meta_drv; };
     in
-    { inherit overlays; inherit meta; inherit namespace_meta; } // flake-utils.lib.eachDefaultSystem (system: rec
+    { inherit overlays; } // flake-utils.lib.eachDefaultSystem (system: rec
     {
-      legacyPackages = import nixpkgs { inherit system; inherit overlays; };
+      legacyPackages = import nixpkgs { inherit system; overlays = nix-proto.lib.overlayToList overlays; };
     });
 }
