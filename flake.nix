4953527bbee9d5d326e6443e28828011e08f0c94
Timothy Gallion <timbama@gmail.com>
2024-02-22 19:09:34 -0500

    Add unnamespaced example and cleanup
    Supports the changes to the python generator to support unnamesapced
    protos with no package.

--- flake.nix
+++ flake.nix
@@ -1,28 +1,58 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
     nix-proto.url = "github:notalltim/nix-proto";
     nix-proto.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, nix-proto, ... }@inputs:
-    let
-      namespace_meta_drv = nix-proto.mkProtoDerivation {
-        name = "name_stuff";
-        src = nix-proto.lib.srcFromNamespace { root = ./proto; namespace = "outer/inner/namespaced"; };
-        version = "1.0.0";
-        protoDeps = [ ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nix-proto,
+    ...
+  } @ inputs: let
+    namespace_meta_drv = nix-proto.mkProtoDerivation {
+      name = "name_stuff";
+      src = nix-proto.lib.srcFromNamespace {
+        root = ./proto;
+        namespace = "outer/inner/namespaced";
       };
-      meta_drv = { name_stuff }: nix-proto.mkProtoDerivation {
+      version = "1.0.0";
+      protoDeps = [];
+    };
+    meta_drv = {
+      name_stuff,
+      unnamespaced,
+    }:
+      nix-proto.mkProtoDerivation {
         name = "upstream";
-        src = nix-proto.lib.srcFromNamespace { root = ./proto; namespace = "upstream/v1"; };
+        src = nix-proto.lib.srcFromNamespace {
+          root = ./proto;
+          namespace = "upstream/v1";
+        };
         version = "1.0.0";
-        protoDeps = [ name_stuff ];
+        protoDeps = [name_stuff unnamespaced];
       };
-      overlays = nix-proto.generateOverlays' { name_stuff = namespace_meta_drv; upstream = meta_drv; };
-    in
-    { inherit overlays; } // flake-utils.lib.eachDefaultSystem (system: rec
-    {
-      legacyPackages = import nixpkgs { inherit system; overlays = nix-proto.lib.overlayToList overlays; };
-    });
+
+    unnamepaced_meta = nix-proto.mkProtoDerivation {
+      name = "unnamespaced";
+      src = ./proto2;
+      version = "0.0.0";
+      protoDeps = [];
+    };
+    overlays = nix-proto.generateOverlays' {
+      name_stuff = namespace_meta_drv;
+      upstream = meta_drv;
+      unnamespaced = unnamepaced_meta;
+    };
+  in
+    {inherit overlays;}
+    // flake-utils.lib.eachDefaultSystem (system: rec
+      {
+        legacyPackages = import nixpkgs {
+          inherit system;
+          overlays = nix-proto.lib.overlayToList overlays;
+        };
+      });
 }
